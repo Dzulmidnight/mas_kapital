3030445c342272f36182a5bdc6cb1b1baa3e51dd
Dzulmidnight <yasser.midnight@gmail.com>
2017-07-11 21:35:30 -0500

    modificación 11_07_2017

--- administracion/index.php
+++ administracion/index.php
@@ -95,29 +95,8 @@
 
   <script>
 
-      //owl carousel
 
-      $(document).ready(function() {
-          $("#owl-demo").owlCarousel({
-              navigation : true,
-              slideSpeed : 300,
-              paginationSpeed : 400,
-              singleItem : true,
-			  autoPlay:true
 
-          });
-      });
-
-      //custom select box
-
-      $(function(){
-          $('select.styled').customSelect();
-      });
-
-      $(window).on("resize",function(){
-          var owl = $("#owl-demo").data("owlCarousel");
-          owl.reinit();
-      });
 
   </script>
 
